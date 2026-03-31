38a5328213497a28c243920a9b8b0030e7213df2
bilic <jopab03@gmail.com>
2026-03-31 20:23:43 +0200

    add cart context

---
+++ fe/src/providers/cart/cartContext.tsx
@@ -0,0 +1,67 @@
+import { createContext, useState, type ReactNode } from "react";
+import type { Size } from "../../types/enums/size";
+import client from "../../api/client";
+
+interface CartItem {
+  productId: number;
+  quantity: number;
+  selectedSize: Size;
+}
+
+interface CartContextType {
+  items: CartItem[];
+  addItem: (item: CartItem) => void;
+  removeItem: (item: CartItem) => void;
+  clearCart: () => void;
+  placeOrder: () => Promise<void>;
+}
+
+export const CartContext = createContext<CartContextType | undefined>(
+  undefined,
+);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [items, setItems] = useState<CartItem[]>([]);
+
+  const addItem = (item: CartItem) => {
+    setItems((prevItems) => [...prevItems, item]);
+  };
+
+  const removeItem = (item: CartItem) => {
+    setItems((prevItems) =>
+      prevItems.filter(
+        (i) =>
+          i.productId !== item.productId ||
+          i.selectedSize !== item.selectedSize ||
+          i.quantity !== item.quantity,
+      ),
+    );
+  };
+  const clearCart = () => {
+    setItems([]);
+  };
+
+  const placeOrder = async () => {
+    const { data, error } = await client.post("/orders", {
+      items: items.map((item) => ({
+        productId: item.productId,
+        quantity: item.quantity,
+        selectedSize: item.selectedSize,
+      })),
+    });
+
+    if (error) {
+      throw new Error(error);
+    }
+    alert("Order placed successfully!");
+    clearCart();
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ items, addItem, removeItem, clearCart, placeOrder }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
